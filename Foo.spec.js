804827057f2d60566ad15860b5218d7cc3c8068f
quentine <qt911025@qq.com>
2020-07-25 18:05:11 +0800

    remove irrelevant components

--- Foo.spec.js
+++ Foo.spec.js
@@ -1,18 +1,11 @@
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
 import Foo from './Foo'
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import { createElement } from './__test.utils.env'
-
-// Vue.use(Vuetify)
-// const vuetify = new Vuetify()
 
 describe('foo', () => {
   it('1', () => {
     const wrapper = mount(Foo, {
-      localVue: Vue,
-      attachTo: createElement(),
-      // vuetify,
+      localVue: createLocalVue(),
       sync: false,
     })
 
