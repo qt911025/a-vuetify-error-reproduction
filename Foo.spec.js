58a96270dfb6e1d2c468c27cb734360a0686210a
quentine <qt911025@qq.com>
2020-07-25 17:57:25 +0800

    remove vuetify

--- Foo.spec.js
+++ Foo.spec.js
@@ -4,15 +4,15 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import { createElement } from './__test.utils.env'
 
-Vue.use(Vuetify)
-const vuetify = new Vuetify()
+// Vue.use(Vuetify)
+// const vuetify = new Vuetify()
 
 describe('foo', () => {
   it('1', () => {
     const wrapper = mount(Foo, {
       localVue: Vue,
       attachTo: createElement(),
-      vuetify,
+      // vuetify,
       sync: false,
     })
 
